0e58b9b0634ab7e01006fc7cb8b682c6a47fc32c
hlourtie <henry@novutech.com>
2020-12-26 15:13:19 +0100

    minor changes to output

--- WSBscrapper.py
+++ WSBscrapper.py
@@ -1,8 +1,6 @@
-from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import sys
-
 
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
@@ -12,7 +10,6 @@ driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=options)
 for x in range(len(sys.argv)):
 	if x == 0:
 		continue
-	print('https://www.reddit.com/r/wallstreetbets/search?q='+sys.argv[x]+'&restrict_sr=1&sort=new')
 	driver.get('https://www.reddit.com/r/wallstreetbets/search?q='+sys.argv[x]+'&restrict_sr=1&sort=new')
 	posts = driver.find_elements_by_xpath('//div[contains(@class,"scrollerItem")]')
 	titles = []
@@ -31,11 +28,9 @@ for x in range(len(sys.argv)):
 	for i in range(len(titles)):
 		print("----")
 		print(titles[i])
-		text = "The link"
+		text = "The link: "
 		target = links[i]
-		print(f"\u001b]8;;{target}\u001b\\{text}\u001b]8;;\u001b\\")
+		print(f"\u001b]8;;{target}\u001b\\{text}\u001b]8;;\u001b\\" + target)
 		print(flairs[i])
 		print("----\n")
 driver.quit()
-#_2X6EB3ZhEeXCh1eIVA64XM _2hSecp_zkPm_s5ddV2htoj _2VqfzH0dZ9dIl3XWNxs42y aJrgrewN9C8x1Fusdx4hh
-#_2X6EB3ZhEeXCh1eIVA64XM _2hSecp_zkPm_s5ddV2htoj _2VqfzH0dZ9dIl3XWNxs42y aJrgrewN9C8x1Fusdx4hh 
